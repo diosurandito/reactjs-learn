bc2bd77c638e8f0547bff6d5a6f8a711377cd719
diosurandito <diosurandito@gmail.com>
2020-02-18 10:37:37 +0700

    007_#9-4_interaksi_dengan_backend,pemanggilan_API(DELETE)

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -7,6 +7,22 @@ class BlogPost extends Component {
     state = {
         post: []
     }
+    getPostAPI = () => {
+        axios.get('http://localhost:3004/posts')
+        .then((result)=> {
+            this.setState({
+                post: result.data
+            })
+        })
+    }
+
+    handleRemove = (data) => {
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res)=> {
+            this.getPostAPI()
+           
+        })
+        
+    }
 
     componentDidMount(){
         // fetch('https://jsonplaceholder.typicode.com/posts')
@@ -16,12 +32,7 @@ class BlogPost extends Component {
         //         post: json
         //     })
         // })
-        axios.get('http://localhost:3004/posts')
-        .then((result)=> {
-            this.setState({
-                post: result.data
-            })
-        })
+        this.getPostAPI();
 
     }
 
@@ -31,7 +42,7 @@ class BlogPost extends Component {
             <p className="section-title">Blog Post</p>
             {
                 this.state.post.map(post => {
-                    return <Post key={post.id} title={post.title} desc={post.body} />
+                    return <Post key={post.id} data={post} remove={this.handleRemove} />
                 })
             }
             </Fragment>
